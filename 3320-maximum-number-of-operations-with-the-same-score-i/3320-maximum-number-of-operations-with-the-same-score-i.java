7897d39fa16c7a8f4f1147b8ae0d4ba45f0a08c5
AdiDayat12 <148147468+AdiDayat12@users.noreply.github.com>
2024-07-15 19:51:04 +0300

    Time: 0 ms (100%), Space: 41.6 MB (50.5%) - LeetHub

---
+++ 3320-maximum-number-of-operations-with-the-same-score-i/3320-maximum-number-of-operations-with-the-same-score-i.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxOperations(int[] nums) {
+      if (nums.length <= 1){
+            return 0;
+        }
+        if (nums.length <= 3){
+            return 1;
+        }
+        int sum = nums[0] + nums[1];
+        int count = 1;
+        for (int i = 2; i < nums.length - 1; i+=2) {
+            if (nums[i] + nums[i + 1] == sum){
+                count++;
+            }else {
+                return count;
+            }
+        }
+        return count;  
+    }
+}
