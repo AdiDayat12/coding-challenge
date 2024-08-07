2c58bdbf84d1f96252771fc0b2548e011f5de920
AdiDayat12 <148147468+AdiDayat12@users.noreply.github.com>
2024-08-07 23:43:22 +0300

    Time: 5 ms (5.49%), Space: 40.6 MB (18.77%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -1,9 +1,9 @@
 class Solution {
     public int findComplement(int num) {
         String bin = Integer.toBinaryString(num);
-        String result1 = bin.replaceAll("0", "5");
-        String result2 = result1.replaceAll("1", "0");
-        String finalResult = result2.replaceAll("5", "1");
-        return Integer.parseInt(finalResult, 2);
+        String result = bin.replaceAll("0", "5")
+                .replaceAll("1", "0")
+                .replaceAll("5", "1");
+        return Integer.parseInt(result, 2);
     }
 }
